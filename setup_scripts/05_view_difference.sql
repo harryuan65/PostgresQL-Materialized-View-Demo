8e2a60b218da8ba1b60912dd0db2d38687b7e600
Harry Yuan <harryuan.65@gmail.com>
2021-04-17 21:31:50 +0800

    feat: update readme and script

--- setup_scripts/05_view_difference.sql
+++ setup_scripts/05_view_difference.sql
@@ -1,12 +1,16 @@
 -- 看下當 insert 新資料時，兩種 view 拉出來的資訊
 
-select * from user_video_report_mv where user_id=1;
-select * from user_video_report_v where user_id=1;
+select * from user_video_report_mv where user_id=2;
+select * from user_video_report_v where user_id=2;
 
-insert into video_watching_histories(user_id, video_id, start_time, end_time) values(1, 3, '2021-04-13 21:43:53 +08:00', '2021-04-13 22:33:03 +08:00');
+insert into video_watching_histories(user_id, video_id, start_time, end_time) values(1, 1658, '2021-04-13 21:43:53 +08:00', '2021-04-14 01:33:03 +08:00');
+insert into video_watching_histories(user_id, video_id, start_time, end_time) values(1, 1658, '2021-04-13 21:43:53 +08:00', '2021-04-14 01:33:03 +08:00', '2021-04-13 21:43:53 +08:00', '2021-04-13 21:43:53 +08:00');
+
+select * from user_video_report_mv where user_id=2;
+select * from user_video_report_v where user_id=2;
 
 refresh materialized view user_video_report_mv;
 
-select * from user_video_report_v where user_id=1;
-select * from user_video_report_mv where user_id=1;
+select * from user_video_report_v where user_id=2;
+select * from user_video_report_mv where user_id=2;
 
